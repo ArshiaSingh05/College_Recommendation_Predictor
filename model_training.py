cae8870510231d9af2e9866782a050dfab48b586
ArshiaSingh05 <singharshia2005@gmail.com>
2025-03-10 19:04:01 +0530

    Saved trained model as model.pkl

--- model_training.py
+++ model_training.py
@@ -1,7 +1,7 @@
 import pandas as pd
 from sklearn.model_selection import train_test_split
 from sklearn.linear_model import LinearRegression
-from sklearn.metrics import mean_squared_error, r2_score
+from sklearn.metrics import mean_squared_error, mean_absolute_error, r2_score
 import joblib
 
 data = pd.read_csv("cleaned_data.csv")
@@ -19,3 +19,19 @@ print(f"R-squared (R²): {r2}")
 
 #Saving the model to reuse it without retraining
 joblib.dump(model, 'trained_model.pkl')
+model = joblib.load('training_model.pkl')
+# Predictions
+y_pred = model.predict(X_test)
+
+# Mean Squared Error (MSE) - Measures average squared errors
+mse = mean_squared_error(y_test, y_pred)
+
+# Mean Absolute Error (MAE) - Measures average absolute errors
+mae = mean_absolute_error(y_test, y_pred)
+
+# R² Score - Measures how well your model explains the variance in the data
+r2 = r2_score(y_test, y_pred)
+
+print(f"Mean Squared Error (MSE): {mse}")
+print(f"Mean Absolute Error (MAE): {mae}")
+print(f"R² Score: {r2}")
