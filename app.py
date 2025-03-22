1f22b430408bd08cfd4155632e9f9fa2c31f946b
ArshiaSingh05 <singharshia2005@gmail.com>
2025-03-22 09:35:28 +0530

    Fixed col 7 with bar chart

--- app.py
+++ app.py
@@ -238,7 +238,7 @@ else:
 
     with col7:
         st.write("### 🏛️ PG Fee - Bar Chart")
-        filtered_pg_data=filtered_data[filtered_data['PG Fee (Scaled)']>=pg_fee_scaled]
+        filtered_pg_data=filtered_data[filtered_data['PG Fee (scaled)']>=pg_fee_scaled]
         if filtered_fee_data.empty:
             st.warning("⚠ No colleges found with this UG Fee.")
         else:
